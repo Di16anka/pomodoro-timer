7d04ff857570f36affc3c97af192e2d13a976368
Di16anka <diana.pasekova@rambler.ru>
2025-08-13 21:39:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
 let timer;
 let minutes = 25;
 let seconds = 0;
+let isStarted = false;
 
+const timerAction = document.querySelector('#pomodoro-time');
+const toStart = document.querySelector('#start');
+
+toStart.addEventListener('click', startTimer);
 function startTimer() {
-    timer = setInterval(updateTimer, 1000);
+    if(isStarted==true) {
+    isStarted = false;
+    toStart.textContent = "stop";
+    } else {
+    isStarted = true;
+    toStart.textContent = "start";
+    }
+    timer = setInterval(updateTimer, 10);
+    
 }
 
 function updateTimer() {
-    const timerElement =
-        document.querySelector('#pomodoro-time');
-    timerElement.textContent = 
-        formatTime(minutes, seconds);
-
+    timerAction.textContent = formatTime(minutes, seconds);
     if (minutes === 0 && seconds === 0) {
-        clearInterval(timer);
-   } else {
-        if (seconds > 0) {
+        clearInterval(timer);  
+   } else 
+   if (!isStarted) {
+    if (seconds > 0) {
             seconds--;
         } else {
             seconds = 59;
             minutes--;
         }
-    }
+    }  
 }
 
 function formatTime(minutes, seconds) {
-    `${minutes}:${seconds}`;
-    return
+    return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
 }
 
 
-function togglePauseResume() {
-    const pauseResumeButton =
-        document.querySelector('#break');
-    
-        clearInterval(timer);
-        pauseResumeButton.textContent = 'reset';
-
-        startTimer();
-        pauseResumeButton.textContent = 'start';
-}
 startTimer();
